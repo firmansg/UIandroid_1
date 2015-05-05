0ae51b1b382872a64906a15048d61ec0c37af106
firmansg <firman2008@ymail.com>
2015-05-05 09:45:05 +0700

    ui 1.0

---
+++ src/com/android/uiandroid1/UIandroid1.java
@@ -0,0 +1,22 @@
+package com.android.uiandroid1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class UIandroid1 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_uiandroid1);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.uiandroid1, menu);
+		return true;
+	}
+
+}
