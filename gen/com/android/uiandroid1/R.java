8ce7c97db973f4ee0eeea7d9fe872d4a87a74cd0
firmansg <firman2008@ymail.com>
2015-05-05 10:23:49 +0700

    ui 3.0

--- gen/com/android/uiandroid1/R.java
+++ gen/com/android/uiandroid1/R.java
@@ -24,9 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int button=0x7f080001;
-        public static final int text=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int loginbutton=0x7f080002;
+        public static final int password=0x7f080001;
+        public static final int status=0x7f080003;
+        public static final int username=0x7f080000;
     }
     public static final class layout {
         public static final int activity_uiandroid1=0x7f030000;
